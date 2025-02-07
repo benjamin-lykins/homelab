ba65b3d84a3a06d0dd9dcecb16cf4c357918ffb4
Benjamin Lykins <benjamin.m.lykins@gmail.com>
2025-02-07 14:23:51 -0500

    Refactor Terraform configuration files to comment out resource definitions for clients and server

--- terraform/client.tf
+++ terraform/client.tf
@@ -1,61 +1,61 @@
-locals {
-  client_ip_addresses = {
-    "mf1" = "192.168.39.120/24"
-    "mf2" = "192.168.39.121/24"
-    "mf3" = "192.168.39.122/24"
-  }
-}
+# locals {
+#   client_ip_addresses = {
+#     "mf1" = "192.168.39.120/24"
+#     "mf2" = "192.168.39.121/24"
+#     "mf3" = "192.168.39.122/24"
+#   }
+# }
 
-resource "proxmox_virtual_environment_vm" "clients" {
-  for_each  = toset(data.proxmox_virtual_environment_nodes.available_nodes.names)
-  node_name = each.key
-  name      = "client-${each.key}"
-  tags      = ["terraform", "client"]
+# resource "proxmox_virtual_environment_vm" "clients" {
+#   for_each  = toset(data.proxmox_virtual_environment_nodes.available_nodes.names)
+#   node_name = each.key
+#   name      = "client-${each.key}"
+#   tags      = ["terraform", "client"]
 
-  memory {
-    dedicated = 4096
-  }
+#   memory {
+#     dedicated = 4096
+#   }
 
-  initialization {
-    user_account {
-      username = var.username
-      password = var.password
-    }
-    ip_config {
-      ipv4 {
-        address = lookup(local.client_ip_addresses, each.key, "192.168.39.120/24")
-        gateway = "192.168.39.1"
-      }
-    }
-  }
-  clone {
-    node_name = "mf2"
-    vm_id     = 202
-    full      = true
-  }
+#   initialization {
+#     user_account {
+#       username = var.username
+#       password = var.password
+#     }
+#     ip_config {
+#       ipv4 {
+#         address = lookup(local.client_ip_addresses, each.key, "192.168.39.120/24")
+#         gateway = "192.168.39.1"
+#       }
+#     }
+#   }
+#   clone {
+#     node_name = "mf2"
+#     vm_id     = 202
+#     full      = true
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Waiting for cloud-init to complete...'",
-      "cloud-init status --wait > /dev/null",
-      "echo 'Completed cloud-init!'",
-    ]
+#   provisioner "remote-exec" {
+#     inline = [
+#       "echo 'Waiting for cloud-init to complete...'",
+#       "cloud-init status --wait > /dev/null",
+#       "echo 'Completed cloud-init!'",
+#     ]
 
-    connection {
-      type     = "ssh"
-      host     = "192.168.39.100"
-      user     = var.username
-      password = var.password
-    }
-  }
+#     connection {
+#       type     = "ssh"
+#       host     = "192.168.39.100"
+#       user     = var.username
+#       password = var.password
+#     }
+#   }
 
 
-  lifecycle {
-    ignore_changes = [
-      network_device, vga
-    ]
-  }
-  depends_on = [
-    proxmox_virtual_environment_vm.server
-  ]
-}
+#   lifecycle {
+#     ignore_changes = [
+#       network_device, vga
+#     ]
+#   }
+#   depends_on = [
+#     proxmox_virtual_environment_vm.server
+#   ]
+# }
--- terraform/server.tf
+++ terraform/server.tf
@@ -1,48 +1,48 @@
-resource "proxmox_virtual_environment_vm" "server" {
-  node_name = "mf2"
-  name      = "server"
-  tags      = ["terraform", "server"]
+# resource "proxmox_virtual_environment_vm" "server" {
+#   node_name = "mf2"
+#   name      = "server"
+#   tags      = ["terraform", "server"]
 
-  memory {
-    dedicated = 4096
-  }
+#   memory {
+#     dedicated = 4096
+#   }
 
-  initialization {
-    user_account {
-      username = var.username
-      password = var.password
-    }
-    ip_config {
-      ipv4 {
-        address = "192.168.39.100/24"
-        gateway = "192.168.39.1"
-      }
-    }
-  }
-  clone {
-    node_name = "mf3"
-    vm_id     = 201
-    full      = true
-  }
+#   initialization {
+#     user_account {
+#       username = var.username
+#       password = var.password
+#     }
+#     ip_config {
+#       ipv4 {
+#         address = "192.168.39.100/24"
+#         gateway = "192.168.39.1"
+#       }
+#     }
+#   }
+#   clone {
+#     node_name = "mf3"
+#     vm_id     = 201
+#     full      = true
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Waiting for cloud-init to complete...'",
-      "cloud-init status --wait > /dev/null",
-      "echo 'Completed cloud-init!'",
-    ]
+#   provisioner "remote-exec" {
+#     inline = [
+#       "echo 'Waiting for cloud-init to complete...'",
+#       "cloud-init status --wait > /dev/null",
+#       "echo 'Completed cloud-init!'",
+#     ]
 
-    connection {
-      type     = "ssh"
-      host     = "192.168.39.100"
-      user     = var.username
-      password = var.password
-    }
-  }
+#     connection {
+#       type     = "ssh"
+#       host     = "192.168.39.100"
+#       user     = var.username
+#       password = var.password
+#     }
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      network_device, vga
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       network_device, vga
+#     ]
+#   }
+# }
